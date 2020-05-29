94a589ddda4ccc5386c8543f026395e1d4131b60
unknown <bboinnovate20@gmail.com>
2020-05-29 15:18:12 +0100

    Made a new changes for day 4

--- js/index.js
+++ js/index.js
@@ -181,22 +181,24 @@ function initMap() {
     });
 
     infoWindow = new google.maps.InfoWindow();
-    displayStores()
-    showStoresMarkers()
-    customizeMaker()
+    searchStores()
+
 }
 
 
-function displayStores() {
+function displayStores(stores) {
 
     // create the actual data creating the html
     var storesHtml = "";
+
 
     // loop throught the data
     stores.forEach(function(store, index) {
         // console.log(store);
         var address = store.addressLines;
         var phone = store.phoneNumber;
+
+
 
         storesHtml += `
             <div class="store-container">
@@ -213,11 +215,21 @@ function displayStores() {
             </div>
         `
     });
+    if (!storesHtml) {
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-info">
+                    <h3> OPP! NOT FOUND </h3>
+                    <p> No result corresponding to your search </p>
+                </div>
+             </div>
+        `
+    }
 
     document.querySelector(".store-list").innerHTML = storesHtml;
 }
 
-function showStoresMarkers() {
+function showStoresMarkers(stores) {
     var bounds = new google.maps.LatLngBounds();
     stores.forEach(function(store, index) {
         var latlng = new google.maps.LatLng(
@@ -231,12 +243,12 @@ function showStoresMarkers() {
         var lng = store.coordinates.longitude;
         var lat = store.coordinates.latitude;
         bounds.extend(latlng);
-        createMarker(latlng, name, address, phone, openStatus, lng, lat);
+        createMarker(latlng, name, address, phone, openStatus, lng, lat, index);
     })
     map.fitBounds(bounds);
 }
 
-function createMarker(latlng, name, address, phone, openStatus, lng, lat) {
+function createMarker(latlng, name, address, phone, openStatus, lng, lat, index) {
     var html = `<div class="marker-map">
     <div class="arrow"> <img src="asset/polygon.png" alt=""> </div>
     <h3>${name}</h3>
@@ -244,7 +256,7 @@ function createMarker(latlng, name, address, phone, openStatus, lng, lat) {
         <span class="time">${openStatus}</span>
         <div class="col-2">
             <span class="icon"> <img src="asset/send.png" alt=""> </span>
-            <span class="marker-address"> <a href="https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}"> ${address}</a> </span>
+            <span class="marker-address"> <a target="_blank" href="https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}"> ${address}</a> </span>
         </div>
         <div class="col-2">
             <span class="icon"> <img src="asset/phone.svg" alt=""> </span>
@@ -252,26 +264,106 @@ function createMarker(latlng, name, address, phone, openStatus, lng, lat) {
         </div>
     </div>
 </div>`;
+
     var marker = new google.maps.Marker({
         map: map,
-        position: latlng
+        position: latlng,
+        // icon: "https://i.ibb.co/w4crKdv/marker.png",
+        icon: "asset/icon.png",
+        label: `${index + 1}`
     });
+
     google.maps.event.addListener(marker, 'click', function() {
         infoWindow.setContent(html);
         infoWindow.open(map, marker);
+
+
     });
     markers.push(marker);
 }
 
 
 
-function customizeMaker() {
-    stores.forEach(function(store, index) {
+// function customizeMaker() {
+//     stores.forEach(function(store, index) {
 
-        var marker = new google.maps.Marker({
-            position: { lat: store.coordinates.latitude, lng: store.coordinates.longitude },
-            map: map,
-            icon: "https://i.ibb.co/w4crKdv/marker.png"
+//         var marker = new google.maps.Marker({
+//             position: { lat: store.coordinates.latitude, lng: store.coordinates.longitude },
+//             map: map,
+//             icon: "https://i.ibb.co/w4crKdv/marker.png"
+//         })
+//     })
+// }
+
+
+
+function setsOnClickListener() {
+    var storeElement = document.querySelectorAll('.store-container');
+    storeElement.forEach(function(elem, index) {
+        elem.addEventListener('click', function() {
+            google.maps.event.trigger(markers[index], 'click');
         })
     })
+}
+
+function searchStores() {
+    var foundStore = [];
+    var zipCode = document.getElementById('zip-code-input').value;
+    var name = document.getElementById('zip-code-input').value;
+    var regex = /^[a-zA-Z]+$/;
+
+    if (zipCode && !zipCode.match(regex)) {
+        stores.forEach(function(store) {
+            var postal = store.address.postalCode.substring(0, 5);
+            if (postal == zipCode) {
+                foundStore.push(store);
+            }
+        })
+    } else {
+        // foundStore = stores;
+        if (name && name.match(regex)) {
+            stores.forEach(function(store) {
+                var namestore = store.name;
+                if (containsWord(namestore.toLowerCase(), name.toLowerCase())) {
+                    foundStore.push(store);
+                }
+            })
+        } else {
+            foundStore = stores;
+        }
+    }
+
+
+    clearLocation()
+    showStoresMarkers(foundStore);
+    displayStores(foundStore)
+    setsOnClickListener()
+}
+
+
+
+
+function clearLocation() {
+    infoWindow.close()
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers.length = 0;
+}
+
+
+function containsWord(string, word) {
+    return new RegExp('(?:[^.\w]|^|^\\W+)' + word + '(?:[^.\w]|\\W(?=\\W+|$)|$)').test(string);
+}
+
+
+setTimeout(function() {
+    var element = document.getElementById("pop-up")
+    element.classList.remove("opacity-change");
+}, 4000);
+
+
+function closebutton() {
+    var element = document.getElementById("pop-up")
+    element.classList.remove("opacity-change");
 }
